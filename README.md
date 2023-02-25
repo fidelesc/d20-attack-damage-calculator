850b80b8afc59cb46fe87695e1f4c80b9c231b4a
Lucas <lfidelescosta@gmail.com>
2023-02-24 20:01:55 -0500

    Added player feats

--- README.md
+++ README.md
@@ -15,14 +15,18 @@ The calculator uses Python's `random` module and the `tkinter` library to create
 To use the D&D calculator, simply input the required parameters in the input fields, and click on the "Calculate" button to get the estimated damage output. 
 
 The input fields are:
-- `Attack Condition`: Normal (No advantage or disadvantage), Advantage (Roll two d20s and take the higher result), Disadvantage (Roll two d20s and take the lower result), Enemy paralyzed within 5 ft (The attack roll has advantage and any hit is a critical hit).
+- `Attacks per Turn`: The number of attacks the player makes per turn.
+- `Attack Bonus`: The attack bonus is the bonus to the player's attack roll.
+- `Attack Description`: A description of the player's attack, which includes the number of dice, dice size, and modifier. For example, "2d6+3" means the player rolls 2 six-sided dice and adds 3 to the result.
+
 - `Enemy AC`: The armor class (AC) of the enemy, which is the value that a player must roll to hit them.
 - `Enemy CR`: The enemy CR to get either the mean or median value for AC. Accepts 0,1/8,1/4,1/2,1,2,...,30.
-- `Attack Bonus`: The attack bonus is the bonus to the player's attack roll.
-- `Attacks per Turn`: The number of attacks the player makes per turn.
-- `Extra Damage`: (OPTIONAL) Extra damage roll (for example "2d6+3"), and option to apply "on hit" (every hit), "on critical" (added average damage, NOT CRITICAL, on a critical hit), and "on hit once per turn" is a damage applied once per turn if a hit happens.
-- `Attack Description`: A description of the player's attack, which includes the number of dice, dice size, and modifier. For example, "2d6+3" means the player rolls 2 six-sided dice and adds 3 to the result.
+
+- `Attack Condition`: Normal (No advantage or disadvantage), Advantage (Roll two d20s and take the higher result), Disadvantage (Roll two d20s and take the lower result), Enemy paralyzed within 5 ft (The attack roll has advantage and any hit is a critical hit).
 - `Critical Option`: The option for handling critical hits, which can be set to "Extra roll" or "Maximum damage". While in "Extra roll", the critical hit doubles the number of attack dices (2d6+4 becomes 4d6+4). The "Maximum damage" options adds the maximum dice roll (2d6+4 becomes 2d6+4+12).
+- `Extra Damage`: (OPTIONAL) Extra damage roll (for example "2d6+3"), and option to apply "on hit" (every hit), "on critical" (added average damage, NOT CRITICAL, on a critical hit), and "on hit once per turn" is a damage applied once per turn if a hit happens.
+
+- `Player feats`: Feats that affect the attack roll not included in `Attack Condition`. Note that Elven Accuracy only works if Attack Condition is set to Advantage or Enemy paralyzed.
 
 Once the user clicks on the "Calculate" button, the calculator estimates the average damage output per turn, and displays the result in the output field.
 
--- attack_calculator.py
+++ attack_calculator.py
@@ -1,5 +1,6 @@
 import random
 import tkinter as tk
+from tkinter import ttk
 
 def get_enemy_ac(option, cr):
     """ Get the enemy AC by mean or median from multiple source books"""
@@ -19,26 +20,31 @@ def get_enemy_ac(option, cr):
     '22': 19,'23': 21,'24': 20.5,'25': 20.5,'26': 22,'28': 23.5,'30': 25
     }
 
-    if option=="Mean per CR":
+    if option=="Mean AC per CR":
         try:
             ac = mean_ac[cr]
         except:
             pass
-    elif option=="Median per CR":
+    elif option=="Median AC per CR":
         try:
             ac = median_ac[cr]
         except:
             pass
     return ac
 
-def test_attack_roll(ac, attack_bonus, condition):
+def test_attack_roll(ac, attack_bonus, condition, player):
     """Test the attack roll for a given AC and attack bonus. Accepts different attacking conditions."""
     if condition=="Normal":
         d20_roll = random.randint(1, 20)  # Roll a d20
     elif condition == "Advantage"  or condition=="Enemy paralyzed within 5 ft":
         d20_roll = max(random.randint(1, 20), random.randint(1, 20))  # Roll two d20s and take the higher result
+        if player[0] == True: # Elven Accuracy
+            d20_roll = max(d20_roll, random.randint(1, 20))
     elif condition == "Disadvantage":
         d20_roll = min(random.randint(1, 20), random.randint(1, 20))  # Roll two d20s and take the lower result
+
+    if player[1]==True and d20_roll == 1:
+        d20_roll = random.randint(1, 20)  # Reroll a d20 for halfing luck
 
     if d20_roll == 20:
         return 2  # Critical hit!
@@ -50,7 +56,7 @@ def test_attack_roll(ac, attack_bonus, condition):
         return 0  # Critical miss or miss :(
 
 
-def get_damage(attacks_per_turn, ac, attack_bonus, condition, num_dice, dice_size, modifier, critical_option, extra_option, ex_num_dice, ex_dice_size, ex_modifier):
+def get_damage(attacks_per_turn, ac, attack_bonus, condition, player, num_dice, dice_size, modifier, critical_option, extra_option, ex_num_dice, ex_dice_size, ex_modifier):
     """Get the average damage rate for a given AC and attack bonus."""
     damage_count = 0
     turn = []
@@ -64,7 +70,7 @@ def get_damage(attacks_per_turn, ac, attack_bonus, condition, num_dice, dice_siz
     for i in range(total_rolls):
         damage_per_turn = 0
         for j in range(attacks_per_turn):
-            test = test_attack_roll(ac, attack_bonus, condition)
+            test = test_attack_roll(ac, attack_bonus, condition, player)
             if test==1:
                 if extra_option == "on hit once per turn" and damage_per_turn==0:
                     damage_count += extra_damage
@@ -96,12 +102,12 @@ def get_damage(attacks_per_turn, ac, attack_bonus, condition, num_dice, dice_siz
     average_turn = sum(turn) / len(turn)
     return average_damage, average_turn
     
-def get_success_rate(ac, attack_bonus, condition):
+def get_success_rate(ac, attack_bonus, condition, player):
     """Get the average success rate for a given AC and attack bonus. Accepts different attacking conditions"""
     success_count = 0
     total_rolls = 100000  # Roll 100,000 times to get an accurate average
     for i in range(total_rolls):
-        if test_attack_roll(ac, attack_bonus, condition):
+        if test_attack_roll(ac, attack_bonus, condition, player):
             success_count += 1
     return success_count / total_rolls
 
@@ -135,9 +141,11 @@ def calculate_estimated_damage():
     """Calculate the estimated damage per turn based on the user's input."""
     
     condition = conditions_var.get()
+    player = [checkbox_elven.get(), 
+              checkbox_lucky.get()]
     
     ac_option = ac_option_var.get()
-    if ac_option=="Manual":
+    if ac_option=="Input AC":
         ac = int(ac_entry.get())
     else:
         ac = get_enemy_ac(ac_option, cr_entry.get())
@@ -153,11 +161,12 @@ def calculate_estimated_damage():
     critical_option = critical_option_var.get()
 
     if ac is not None:
-        success_rate = get_success_rate(ac, attack_bonus, condition)
+        success_rate = get_success_rate(ac, attack_bonus, condition, player)
         average_damage, estimated_damage = get_damage(num_attacks, 
                                                       ac, 
                                                       attack_bonus, 
                                                       condition,
+                                                      player,
                                                       num_dice, 
                                                       dice_size, 
                                                       modifier, 
@@ -180,88 +189,124 @@ if __name__ == '__main__':
     root = tk.Tk()
     root.title("d20 Damage Calculator")
     
-    # Create the dropdown menu for selecting game edition
-    edition_label = tk.Label(root, text="Select Attack Condition:")
-    edition_label.grid(row=0, column=0, padx=5, pady=5, columnspan=2)
-    conditions_options = ["Normal", "Advantage", "Disadvantage", "Enemy paralyzed within 5 ft"]
-    conditions_var = tk.StringVar(value=conditions_options[0])
-    condition_menu = tk.OptionMenu(root, conditions_var, *conditions_options)
-    condition_menu.grid(row=0, column=0, padx=5, pady=5, columnspan=5)
-    # Calculate the maximum length of the options
-    max_length = max(len(option) for option in conditions_options)
-    condition_menu.config(width=max_length)
+    # Create the Notebook widget
+    notebook = ttk.Notebook(root)
+    notebook.pack(fill='both', expand=True)
     
-    # Create the input fields
-    ac_label = tk.Label(root, text="Enemy AC:")
-    ac_entry = tk.Entry(root)
-    ac_label.grid(row=1, column=0, padx=5, pady=5)
-    ac_entry.grid(row=1, column=1, padx=5, pady=5)
+    # Create the first tab
+    tab1 = tk.Frame(notebook)
+    notebook.add(tab1, text="Turn Description")
     
-    ac_options = ["Manual","Mean per CR", "Median per CR"]
+    # Create the second tab
+    tab2 = tk.Frame(notebook)
+    notebook.add(tab2, text="Options")
+       
+    # Create the input fields  
+    left_label = tk.Label(tab1, text="Player Attack")
+    left_label.grid(row=0, column=0, padx=5, pady=5, columnspan=2)
+    
+    num_attacks_label = tk.Label(tab1, text="Attacks per Turn:")
+    num_attacks_entry = tk.Entry(tab1)
+    num_attacks_label.grid(row=1, column=0, padx=5, pady=5)
+    num_attacks_entry.grid(row=1, column=1, padx=5, pady=5)  
+    
+    attack_bonus_label = tk.Label(tab1, text="Attack Bonus:")
+    attack_bonus_entry = tk.Entry(tab1)
+    attack_bonus_label.grid(row=2, column=0, padx=5, pady=5)
+    attack_bonus_entry.grid(row=2, column=1, padx=5, pady=5)
+    
+    attack_description_label = tk.Label(tab1, text="Attack Description:")
+    attack_description_entry = tk.Entry(tab1)
+    attack_description_label.grid(row=3, column=0, padx=5, pady=5)
+    attack_description_entry.grid(row=3, column=1, padx=5, pady=5)
+    
+    right_label = tk.Label(tab1, text="Enemy AC or CR")
+    right_label.grid(row=0, column=2, padx=5, pady=5, columnspan=2)
+    
+    ac_options = ["Input AC","Mean AC per CR", "Median AC per CR"]
     ac_option_var = tk.StringVar(value=ac_options[0])
-    ac_option_menu = tk.OptionMenu(root, ac_option_var, *ac_options)
-    ac_option_menu.grid(row=1, column=2, padx=5, pady=5)
+    ac_option_menu = tk.OptionMenu(tab1, ac_option_var, *ac_options)
+    ac_option_menu.grid(row=1, column=2, padx=5, pady=5, columnspan=2)
     # Calculate the maximum length of the options
     max_length = max(len(option) for option in ac_options)
     ac_option_menu.config(width=max_length)
     
-    cr_label = tk.Label(root, text="Enemy CR:")
-    cr_entry = tk.Entry(root)
-    cr_label.grid(row=1, column=3, padx=5, pady=5)
-    cr_entry.grid(row=1, column=4, padx=5, pady=5)
+    ac_label = tk.Label(tab1, text="Enemy AC:")
+    ac_entry = tk.Entry(tab1)
+    ac_label.grid(row=2, column=2, padx=5, pady=5)
+    ac_entry.grid(row=2, column=3, padx=5, pady=5)
     
-    attack_bonus_label = tk.Label(root, text="Attack Bonus:")
-    attack_bonus_entry = tk.Entry(root)
-    attack_bonus_label.grid(row=2, column=0, padx=5, pady=5)
-    attack_bonus_entry.grid(row=2, column=1, padx=5, pady=5)
+    cr_label = tk.Label(tab1, text="Enemy CR:")
+    cr_entry = tk.Entry(tab1)
+    cr_label.grid(row=3, column=2, padx=5, pady=5)
+    cr_entry.grid(row=3, column=3, padx=5, pady=5)
     
-    extra_options = ["on hit","on hit once per turn", "on critical"]
-    extra_option_var = tk.StringVar(value=extra_options[0])
-    extra_option_menu = tk.OptionMenu(root, extra_option_var, *extra_options)
-    extra_option_menu.grid(row=4, column=2, padx=5, pady=5)
+    
+    # Create the button to calculate the estimated damage
+    calculate_button = tk.Button(tab1, text="CALCULATE", command=calculate_estimated_damage)
+    calculate_button.grid(row=4, column=0, columnspan=4, padx=5, pady=5)
+    
+    # Create the label to display the result
+    result_label = tk.Label(tab1, text="")
+    result_label.grid(row=10, column=0, columnspan=5, padx=5, pady=5)
+    result_label2 = tk.Label(tab1, text="")
+    result_label2.grid(row=12, column=0, columnspan=5, padx=5, pady=5)
+    result_label3 = tk.Label(tab1, text="")
+    result_label3.grid(row=14, column=0, columnspan=5, padx=5, pady=5)
+    
+    ### TAB 2
+        
+    # Create the dropdown menu for selecting game edition
+    edition_label = tk.Label(tab2, text="Attack Condition:")
+    edition_label.grid(row=0, column=0, padx=5, pady=5)
+    conditions_options = ["Normal", "Advantage", "Disadvantage", "Enemy paralyzed within 5 ft"]
+    conditions_var = tk.StringVar(value=conditions_options[0])
+    condition_menu = tk.OptionMenu(tab2, conditions_var, *conditions_options)
+    condition_menu.grid(row=0, column=1, padx=5, pady=5)
     # Calculate the maximum length of the options
-    max_length = max(len(option) for option in extra_options)
-    extra_option_menu.config(width=max_length)
+    max_length = max(len(option) for option in conditions_options)
+    condition_menu.config(width=max_length)
     
-    num_attacks_label = tk.Label(root, text="Attacks per Turn:")
-    num_attacks_entry = tk.Entry(root)
-    num_attacks_label.grid(row=2, column=3, padx=5, pady=5)
-    num_attacks_entry.grid(row=2, column=4, padx=5, pady=5)
-    
-    extra_damage_label = tk.Label(root, text="Extra damage:")
-    extra_damage_label.grid(row=4, column=0, padx=5, pady=5, columnspan=2)
-    
-    extra_dmg_label = tk.Label(root, text="Damage description:")
-    extra_dmg_label.grid(row=4, column=3, padx=5, pady=5)
-    extra_option_entry = tk.Entry(root)
-    extra_option_entry.grid(row=4, column=4, padx=5, pady=5)    
-    
-    attack_description_label = tk.Label(root, text="Attack Description:")
-    attack_description_entry = tk.Entry(root)
-    attack_description_label.grid(row=7, column=0, padx=5, pady=5)
-    attack_description_entry.grid(row=7, column=1, padx=5, pady=5)
-    
-    critical_option_label = tk.Label(root, text="Critical Option:")
+    critical_option_label = tk.Label(tab2, text="Critical Option:")
     critical_options = ["Extra roll", "Maximum damage"]
     critical_option_var = tk.StringVar(value=critical_options[0])
-    critical_option_menu = tk.OptionMenu(root, critical_option_var, *critical_options)
-    critical_option_label.grid(row=7, column=3, padx=5, pady=5)
-    critical_option_menu.grid(row=7, column=4, padx=5, pady=5)
+    critical_option_menu = tk.OptionMenu(tab2, critical_option_var, *critical_options)
+    critical_option_label.grid(row=1, column=0, padx=5, pady=5)
+    critical_option_menu.grid(row=1, column=1, padx=5, pady=5)
     # Calculate the maximum length of the options
     max_length = max(len(option) for option in critical_options)
     critical_option_menu.config(width=max_length)
         
-    # Create the button to calculate the estimated damage
-    calculate_button = tk.Button(root, text="Calculate", command=calculate_estimated_damage)
-    calculate_button.grid(row=9, column=2, columnspan=1, padx=5, pady=5)
+    extra_damage_label = tk.Label(tab2, text="Extra damage:")
+    extra_damage_label.grid(row=2, column=0, padx=5, pady=5)
     
-    # Create the label to display the result
-    result_label = tk.Label(root, text="")
-    result_label.grid(row=10, column=0, columnspan=5, padx=5, pady=5)
-    result_label2 = tk.Label(root, text="")
-    result_label2.grid(row=12, column=0, columnspan=5, padx=5, pady=5)
-    result_label3 = tk.Label(root, text="")
-    result_label3.grid(row=14, column=0, columnspan=5, padx=5, pady=5)
+    extra_options = ["on hit","on hit once per turn", "on critical"]
+    extra_option_var = tk.StringVar(value=extra_options[0])
+    extra_option_menu = tk.OptionMenu(tab2, extra_option_var, *extra_options)
+    extra_option_menu.grid(row=2, column=1, padx=5, pady=5)
+    # Calculate the maximum length of the options
+    max_length = max(len(option) for option in extra_options)
+    extra_option_menu.config(width=max_length)
+    
+    extra_dmg_label = tk.Label(tab2, text="Damage description:")
+    extra_dmg_label.grid(row=3, column=0, padx=5, pady=5)
+    extra_option_entry = tk.Entry(tab2)
+    extra_option_entry.grid(row=3, column=1, padx=5, pady=5) 
+    
+    tab2_label1 = tk.Label(tab2, text="Player feats")
+    tab2_label1.grid(row=4, column=0, padx=5, pady=5, columnspan=2)
+    
+    # Create a variable to store the state of the checkbox
+    checkbox_elven = tk.BooleanVar()
+    # Create the checkbox widget and associate it with the variable
+    checkbox1 = tk.Checkbutton(tab2, text="Elven Accuracy", variable=checkbox_elven)
+    checkbox1.grid(row=5, column=0, padx=5, pady=5) 
+    
+    # Create a variable to store the state of the checkbox
+    checkbox_lucky = tk.BooleanVar()
+    checkbox2 = tk.Checkbutton(tab2, text="Halfling Lucky", variable=checkbox_lucky)
+    checkbox2.grid(row=5, column=1, padx=5, pady=5) 
+    
 
     
     # Start the main event loop
